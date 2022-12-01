3e5f0da8662ee17d4c4c2c15a4614741b266ff41
BuildEnough <rhtmaehcl878@naver.com>
2022-12-01 16:21:42 +0900

    perf: 로그인 방식 변경

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ from imagekit.processors import Thumbnail
 
 
 class User(AbstractUser):
+    nickname = models.CharField(max_length=20, unique=True)
     image = ProcessedImageField(
         upload_to="images/",
         blank=True,
