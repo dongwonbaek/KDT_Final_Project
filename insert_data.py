12bb4bb2cd6c2013d6a816901af5f97cea7f9b5a
dongwonbaek <bmrio100@gmail.com>
2022-12-08 16:52:52 +0900

    feat: 모델 분리로 인한 크롤링 데이터 삽입 코드 변경

--- insert_data.py
+++ insert_data.py
@@ -5,15 +5,21 @@ import django
 django.setup()
 
 import json
-from articles.models import Product, ProductImages
-
+from articles.models import Product, ProductImages, ProductContentImages
+# range(1, 12)
 for idx in range(1, 12):
-    with open(f'crawling_data/category{idx}.json', encoding='utf-8') as f:
-        json_objects = json.load(f)
+    if idx != 10:
+        with open(f'crawling_data/category{idx}.json', encoding='utf-8') as f:
+            json_objects = json.load(f)
 
-    if json_objects:
-        for x in json_objects:
-            product = Product(title=json_objects[x]['name'], category=json_objects[x]['category'], price=int(json_objects[x]['price'].replace('원', '').replace(',', '')))
-            productimages = ProductImages(product=product, images=json_objects[x]['img'])
-            product.save()
-            productimages.save()
+        if json_objects:
+            for x in json_objects:
+                product = Product(title=json_objects[x]['name'], product_url=json_objects[x]['url'], brand=json_objects[x]['brand'].replace('\n바로가기', ''), category=json_objects[x]['category'], price=int(json_objects[x]['price'].replace('원', '').replace(',', '')))
+                product.save()
+                for thumbnail_image in json_objects[x]['thumbnail_images']:
+                    productimages = ProductImages(product=product, images=thumbnail_image)
+                    productimages.save()
+                if json_objects[x]['content_images']:
+                    for content_image in json_objects[x]['content_images']:
+                        productcontentimages = ProductContentImages(product=product, images=content_image)
+                        productcontentimages.save()
