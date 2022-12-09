a7f2cbf8818b7283c96ade912e892ea1bf81edf1
BuildEnough <rhtmaehcl878@naver.com>
2022-12-10 01:58:54 +0900

    feat: 커뮤니티 댓글 기능 추가(비동기 x)

--- articles/forms.py
+++ articles/forms.py
@@ -62,7 +62,7 @@ class CommunityImagesForm(forms.ModelForm):
         }
 
 
-# class CommunityCommentForm(forms.ModelForm):
-#     class Meta:
-#         model = CommunityComment
-#         fields = ("content",)
+class CommunityCommentForm(forms.ModelForm):
+    class Meta:
+        model = CommunityComment
+        fields = ["content",]
--- articles/models.py
+++ articles/models.py
@@ -113,6 +113,13 @@ class CommunityImages(models.Model):
         format="JPEG",
         options={"quality": 80},
     )
+    
+class CommunityComment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    community = models.ForeignKey('Community', on_delete=models.CASCADE)
+
+
 
 # class CommunityComment(models.Model):
 #     content = models.TextField()
